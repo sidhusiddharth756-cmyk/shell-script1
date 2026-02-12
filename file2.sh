00acbd3c70d15fba3a2f17b133b7c7c3c033f496
USER <mounigowd80@gmail.com>
2026-02-12 16:24:26 +0530

    updated script

---
+++ file2.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "please enter your username"
+
+reaf USERNAME
+
+echo "username entered is: $USEERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "password entered is: $PASSWORD"
